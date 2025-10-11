a7aa4d0c7c8dcb3f3dfb684b67b9d90a636a9264
ashuhlee <ashleyoye24@gmail.com>
2025-10-11 03:12:25 -0400

    add imports to animation file

--- src/ui/animations.py
+++ src/ui/animations.py
@@ -1,7 +1,31 @@
-from src.imports import *
+import os
+import sys
+import time
+import requests
+from rich.console import Console
+from rich.spinner import Spinner
+from dotenv import load_dotenv
+
+console = Console()
 
 def typewriter(text: str, speed: float):
     for char in text:
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(speed)
+
+def spinner(text: str, speed: float, found: bool):
+
+    print()
+
+    with console.status(f'[cyan]{text}[/cyan]', spinner='dots', spinner_style='cyan'):
+        # length of animation
+        time.sleep(speed)
+
+    if found:
+        console.print('[green]Data fetched successfully![/green]')
+    else:
+        console.print('[red]City not found. Please try again[/red]\n')
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
