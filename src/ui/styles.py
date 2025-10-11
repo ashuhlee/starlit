137d247e5ebd227fa5522f2923aaf5db0fee804e
ashuhlee <ashleyoye24@gmail.com>
2025-10-11 07:39:40 -0400

    add bullet point

--- src/ui/styles.py
+++ src/ui/styles.py
@@ -4,8 +4,12 @@ class Style:
     bold = '\033[1m'
     yellow = '\033[33m'
     magenta = '\033[35m'
+    cyan = '\033[36m'
     green = '\033[32m'
 
 class Colors:
     # hex codes
     purple = '#9598f7'
+
+class Misc:
+    point = f'{Style.yellow}•{Style.end} '
