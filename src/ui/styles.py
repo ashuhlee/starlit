b6d1c6c26fa403fdab170245a0c083b3939484fd
ashuhlee <ashleyoye24@gmail.com>
2025-10-11 04:21:17 -0400

    add hex colors as variables

--- src/ui/styles.py
+++ src/ui/styles.py
@@ -1,6 +1,11 @@
 class Style:
+    # ascii codes
     end = '\033[0m'
     bold = '\033[1m'
     yellow = '\033[33m'
     magenta = '\033[35m'
     green = '\033[32m'
+
+class Colors:
+    # hex codes
+    purple = '#9598f7'
