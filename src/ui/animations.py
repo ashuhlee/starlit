cc79ab2ecfd4698eff67088557cdf5b40d05fd21
ashuhlee <ashleyoye24@gmail.com>
2025-10-11 04:20:42 -0400

    add comments

--- src/ui/animations.py
+++ src/ui/animations.py
@@ -8,12 +8,14 @@ from dotenv import load_dotenv
 
 console = Console()
 
+#typewriter effect for text
 def typewriter(text: str, speed: float):
     for char in text:
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(speed)
 
+# spinning animation for loading data
 def spinner(text: str, speed: float, found: bool):
 
     print()
@@ -23,9 +25,10 @@ def spinner(text: str, speed: float, found: bool):
         time.sleep(speed)
 
     if found:
-        console.print('[green]Data fetched successfully![/green]')
+        console.print('[green]󰄬 Data fetched successfully![/green]')
     else:
-        console.print('[red]City not found. Please try again[/red]\n')
+        console.print('[red] City not found. Please try again[/red]')
 
+# clear terminal screen
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
