2cf6412c26bfefb18b1460e15605885a265af539
ashuhlee <ashleyoye24@gmail.com>
2025-10-13 08:14:58 -0400

    fix error message + add force quit function

--- src/ui/animations.py
+++ src/ui/animations.py
@@ -15,19 +15,28 @@ def typewriter(text: str, speed: float):
         time.sleep(speed)
 
 # spinning animation for loading data
-def spinner(text: str, speed: float, found: bool):
+def spinner(text: str, speed: float, found):
 
     print()
-
     with console.status(f'[cyan]{text}[/cyan]', spinner='dots', spinner_style='magenta'):
         # length of animation
         time.sleep(speed)
 
     if found:
         console.print('[green]󰄬 Data fetched successfully![/green]')
-    else:
+
+    elif found is False:
         console.print('[red] City not found. Please try again[/red]')
+        time.sleep(0.5)
+
+    elif found is None:
+        pass
 
 # clear terminal screen
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+# force quit app
+def force_quit():
+    print('\nForce quitting app.')
+    sys.exit(0)
