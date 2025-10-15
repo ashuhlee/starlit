bd76180c00bfb0ae3302b35d8f500699a5892d54
ashuhlee <ashleyoye24@gmail.com>
2025-10-15 09:02:34 -0400

    add functions for terminal actions

---
+++ src/ui/system_utils.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import time
+import requests
+from dotenv import load_dotenv
+from typing import Any
+
+from rich.console import Console
+
+from ui.styles import Colors, label
+
+hide_cur = '\033[?25l'
+show_cur = '\033[?25h'
+
+console = Console()
+
+# exit app
+def exit_app():
+    print(hide_cur)
+    label('exit', 'Exiting weather-cli..', Colors.pink)
+
+    time.sleep(5), clear_screen()
+
+    label('exit', 'Exited weather-cli', Colors.pink)
+    print(show_cur, end='', flush=True)
+
+    sys.exit(0)
+
+# force quit app
+def force_quit():
+    label('exit', 'Force quitting app', Colors.orange)
+    sys.exit(0)
+
+# clear terminal screen
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
